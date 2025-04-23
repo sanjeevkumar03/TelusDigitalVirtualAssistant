0ff1b9ca39afcb0356a61be0dfd3050a09470b0b
sanjeevkumar03 <91119044+sanjeevkumar03@users.noreply.github.com>
2025-04-23 17:33:28 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "TelusDigitalVirtualAssistant",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -12,10 +13,27 @@ let package = Package(
             targets: ["TelusDigitalVirtualAssistant"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/XMPPFramework.git", from: "4.0.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.18.10"),
+        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser.git", from: "5.6.0"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", from: "7.0.0"),
+        .package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "14.1.0")
+    ],
     targets: [
-        .binaryTarget(
+        .target(
             name: "TelusDigitalVirtualAssistant",
-            path: "./TelusDigitalVirtualAssistant.xcframework"
-        )
+            dependencies: [
+                "XMPPFramework",
+                "SDWebImage",
+                "SwiftyXMLParser",
+                "IQKeyboardManager",
+                "ProgressHUD"
+            ],
+            path: "Sources/TelusDigitalVirtualAssistant",
+            resources: [
+                // .process("Resources") // Uncomment if needed
+            ]
+        ),
     ]
 )
